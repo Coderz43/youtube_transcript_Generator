12a669d93b1ce13f4603e2f37b17be54dad95e32
Coderz43 <noman2675010@gmail.com>
2025-06-06 03:09:54 +0500

    Fix CommonJS import for youtube-transcript

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
-import { getTranscript } from 'youtube-transcript';
+import pkg from 'youtube-transcript';
+const { getTranscript } = pkg;
 
 const app = express();
 app.use(cors());
