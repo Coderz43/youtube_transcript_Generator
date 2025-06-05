31d6e1f090ae2fdbfafc54528f5ad17c08b137bb
Coderz43 <noman2675010@gmail.com>
2025-06-06 01:35:46 +0500

    Update Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -8,11 +8,14 @@ export default defineConfig({
   },
   server: {
     port: 5173,
+    host: true,
     proxy: {
       '/api': {
         target: 'http://localhost:3001',
         changeOrigin: true,
-      },
-    },
-  },
+        secure: false,
+        rewrite: (path) => path.replace(/^\/api/, '')
+      }
+    }
+  }
 });
