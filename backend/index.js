78bb84237a5273e30e4ac2e2de978e9f4e093989
Coderz43 <noman2675010@gmail.com>
2025-06-06 03:47:51 +0500

    Updated index.js

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import cors from 'cors';
-import { getTranscript } from 'youtube-transcript'; // <--- YAHAN CHANGE KAREIN
+import { getTranscript } from 'youtube-transcript'; 
 
 const app = express();
 app.use(cors());
